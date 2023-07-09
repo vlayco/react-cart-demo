e75ea6551f125e31c843b3709658c1d3827db5b5
Vladimir Vlayco <vladaj00@gmail.com>
2023-07-09 18:23:56 +0200

    completed filtering

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,15 +3,50 @@ import SideBar from "../SideBar"
 import ProductCard from "../product/ProductCard"
 
 const Home = () => {
-  const { state: {products} } = CartState()
+  const { 
+    state: {products}, 
+    productState: {
+      sort,
+      searchQuery,
+      byRating,
+      byStock,
+      byFastDelivery
+    }
+  } = CartState()
 
-  // console.log(products)
+  const transformProducts = () => {
+    let sortedProducts = products;
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) => (
+        sort === 'low-to-high' ? a.price - b.price : b.price - a.price
+      ))
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((product) => product.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((product) => product.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter((product) => product.ratings >= byRating);
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((product) => product.name.toLowerCase().includes(searchQuery));
+    }
+
+    return sortedProducts;
+  }
 
   return (
     <div className="container mx-auto mt-2 flex flex-row">
         <SideBar />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 flex-wrap justify-around p-4">
-        {products.map((product) => {
+        {transformProducts().map((product) => {
           return <ProductCard product={product} key={product.uuid} />
         })}
       </div>
